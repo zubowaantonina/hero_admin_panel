d891dcdb5b96b51acf74ad728f3ab7f7bd3455fd
zubowaantonina <zubowaantonina@yandex.ru>
2024-09-21 12:37:17 +0300

    Middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,20 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
+
+const stringMiddleware = () => (next) => (action) => {
+     if (typeof action === 'string') {
+          return next({
+               type: action
+          })
+     }
+     return next(action)
+}
+
+
 //если в dispatch пришла 'string',меняем её тип на объект
 const enhancer = (createStore) => (...args) => {
      const store = createStore(...args);
-
      const oldDispatch = store.dispatch;
      store.dispatch = (action) => {
           if (typeof action === 'string') {
@@ -19,8 +29,10 @@ const enhancer = (createStore) => (...args) => {
 
 const store = createStore(combineReducers({ heroes, filters }),
      compose(
-          enhancer,
+          applyMiddleware(stringMiddleware),
           window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-     ));
+     )
+
+);
 
 export default store;
